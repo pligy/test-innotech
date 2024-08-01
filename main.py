0e7fcf8f0afd70fee4e2affbb9fc5f1fce875ba6
levap-21 <igorplatov2002@mail.ru>
2024-08-01 17:06:25 +0300

    add: work all functional, without config

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form, Request, Depends, HTTPException, status
 from fastapi.responses import RedirectResponse, HTMLResponse, JSONResponse
-from typing import Dict
+import hashlib
 
 app = FastAPI()
 
@@ -11,9 +11,35 @@ appsec_practices = {
     },
     "practice_sast": {
         "description": "Static Application Security Testing",
-        "details": "Analysis of source code for security vulnerabilities."
+        "details": "Analyzes source code or binaries for security vulnerabilities without executing the program."
+    },
+    "practice_dast": {
+        "description": "Dynamic Application Security Testing",
+        "details": "Examines a running application to find security vulnerabilities by simulating external attacks."
+    },
+    "practice_iast": {
+        "description": "Interactive Application Security Testing",
+        "details": "Combines aspects of both SAST and DAST by analyzing applications in real-time while they are running and interacting with the code and environment."
     },
 }
+
+X_AUTH_TOKEN_COOKIE = "X-Auth-Token"
+
+# Function to generate a hashed token
+def generate_token(username: str, password: str) -> str:
+    to_hash = f"{username}:{password}".encode()
+    return hashlib.sha256(to_hash).hexdigest()
+
+async def get_token_or_raise_http_exc(
+    request: Request
+) -> str:
+    cookie_token = request.cookies.get(X_AUTH_TOKEN_COOKIE)
+    if cookie_token:
+        return cookie_token
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Not authenticated. Go to /login",
+    )
 
 @app.get("/", response_class=RedirectResponse)
 async def root():
@@ -28,19 +54,37 @@ async def login_form():
         <label for="password">Password:</label><br>
         <input type="password" id="password" name="password"><br><br>
         <input type="submit" value="Submit">
-    </form> 
+    </form>
     """
 
 @app.post("/login", response_class=HTMLResponse)
-async def login(username: str = Form(...), password: str = Form(...)):
+async def login(request: Request, username: str = Form(...), password: str = Form(...)):
     if username == "admin" and password == "admin":
-        # return "All correct"
-        return RedirectResponse(url="/appsec?key=practice_main")
+        token = generate_token(username, password)
+        response_content = """
+        <html>
+            <head>
+                <meta http-equiv="refresh" content="2;url=/appsec?key=practice_main">
+            </head>
+            <body>
+                <p>All correct. Redirecting to AppSec practices...</p>
+            </body>
+        </html>
+        """
+        response = HTMLResponse(content=response_content)
+        response.set_cookie(
+            X_AUTH_TOKEN_COOKIE,
+            value=token,
+            httponly=True,
+            max_age=86400,  # 1 day
+            expires=86400,
+        )
+        return response
     else:
         return "Incorrect"
 
 @app.get("/appsec", response_class=JSONResponse)
-async def appsec(key: str):
+async def appsec(key: str, token: str = Depends(get_token_or_raise_http_exc)):
     if key in appsec_practices:
         return appsec_practices[key]
     else:
